c820502fada8961ece98715584ae8de681f4083a
youngkiddl <mi.vivallo@duocuc.cl>
2023-06-02 00:40:25 -0400

    registro creado

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Curso } from 'src/app/cursos/interfaces/curso';
+import { CursoService } from 'src/app/cursos/services/curso.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit{
 
+
+  public cursoService = inject(CursoService);
+  public cursos: Curso[] = [];
+  loading: boolean = true;
+
+
+  ngOnInit(): void {
+    this.getCursos();
+  }
+
+
+  getCursos() {
+    this.loading = true;
+    this.cursoService.getCursos().subscribe((cursos) => {
+      this.cursos = cursos;
+      this.loading = false;
+      console.log(this.cursos);
+    });
+  }
 }
